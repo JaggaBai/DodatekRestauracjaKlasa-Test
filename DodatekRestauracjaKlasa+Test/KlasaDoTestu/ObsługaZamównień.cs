ed1c4d0460a935fad0caaaef9118bbb906c6aadd
Jagga Bai <dzagur@post.pl>
2022-01-24 15:15:51 +0100

    Add project files.

---
+++ DodatekRestauracjaKlasa+Test/KlasaDoTestu/ObsługaZamównień.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DodatekRestauracjaKlasa_Test.KlasaDoTestu
+{
+    public class ObsługaZamównień
+    {
+        public User Wykonujący { get; set; }
+        public bool Odwołaj(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+           
+            return false;
+        }
+        public class User
+        {
+            public bool IsAdmin { get; set; }
+        }
+    }
+}
+    
